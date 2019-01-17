3a5dec27ed790cd90f069238090db733e9f12f4a
dcastr5x <davidx.castro.ruiz@intel.com>
2019-01-17 14:35:24 -0600

    wwqs

--- vars/checkNodes.groovy
+++ vars/checkNodes.groovy
@@ -1,3 +1,9 @@
+import hudson.FilePath
+import hudson.model.Node
+import hudson.model.Slave
+import jenkins.model.Jenkins
+import groovy.time.*
+
 @NonCPS
 def call(jenkinsNodes){
     for (Node node in jenkinsNodes) 
