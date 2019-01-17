ff46665246536d7c8181f2f9a9605a3dcc89c1c4
dcastr5x <davidx.castro.ruiz@intel.com>
2019-01-17 11:27:37 -0600

    checkNodes in function

--- vars/checkNodes.groovy
+++ vars/checkNodes.groovy
@@ -4,31 +4,34 @@ import hudson.model.Slave
 import jenkins.model.Jenkins
 import groovy.time.*
 
-Jenkins jenkins = Jenkins.instance
-def jenkinsNodes =jenkins.nodes
-while(1)
-{
-    for (Node node in jenkinsNodes) 
+
+def node_status(){
+    Jenkins jenkins = Jenkins.instance
+    def jenkinsNodes =jenkins.nodes
+    while(1)
     {
-        sleep(1000)
-        // Make sure slave is online
-        if (!node.getComputer().isOffline()) 
-        {           
-            //Make sure that the slave busy executor number is 0.
-            if(node.getComputer().countBusy()==0)
-            {
-                println "'$node.nodeName' can take jobs !!!"
+        for (Node node in jenkinsNodes) 
+        {
+            sleep(1000)
+            // Make sure slave is online
+            if (!node.getComputer().isOffline()) 
+            {           
+                //Make sure that the slave busy executor number is 0.
+                if(node.getComputer().countBusy()==0)
+                {
+                    println "'$node.nodeName' can take jobs !!!"
+                }
+                else
+                {
+                    println "$node.nodeName' is busy !!!"
+                }
             }
             else
             {
-                println "$node.nodeName' is busy !!!"
+                println "'$node.nodeName' is offline !!!" 
             }
         }
-        else
-        {
-            println "'$node.nodeName' is offline !!!" 
-        }
+        sleep(1000)
+        return 0
     }
-    sleep(1000)
-    return 0
 }
