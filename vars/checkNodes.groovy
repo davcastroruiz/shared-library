1264c60a7150a8f0df0bd55e91544888b8b6657b
dcastr5x <davidx.castro.ruiz@intel.com>
2019-01-17 13:11:31 -0600

    xxxx

--- vars/checkNodes.groovy
+++ vars/checkNodes.groovy
@@ -6,30 +6,7 @@ import groovy.time.*
 
 @NonCPS
 def call(jenkinsNodes){
-    while(1)
-    {
-        for (Node node in jenkinsNodes) 
-        {
-            sleep(10)
-            // Make sure slave is online
-            if (!node.getComputer().isOffline()) 
-            {           
-                //Make sure that the slave busy executor number is 0.
-                if(node.getComputer().countBusy()==0)
-                {
-                    println "'$node.nodeName' can take jobs !!!"
-                }
-                else
-                {
-                    println "$node.nodeName' is busy !!!"
-                }
-            }
-            else
-            {
-                println "'$node.nodeName' is offline !!!" 
-            }
-        }
-        sleep(10)
-        return 0
-    }
+    
+    println "can take jobs !!!"
+    
 }
