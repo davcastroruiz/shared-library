87def8d1a51d4b6192024f6ff514ce9ecb860894
dcastr5x <davidx.castro.ruiz@intel.com>
2019-01-17 09:54:13 -0600

    checkNodes groovy script

---
+++ vars/checkNodes.groovy
@@ -0,0 +1,34 @@
+import hudson.FilePath
+import hudson.model.Node
+import hudson.model.Slave
+import jenkins.model.Jenkins
+import groovy.time.*
+
+Jenkins jenkins = Jenkins.instance
+def jenkinsNodes =jenkins.nodes
+while(1)
+{
+    for (Node node in jenkinsNodes) 
+    {
+        sleep(1000)
+        // Make sure slave is online
+        if (!node.getComputer().isOffline()) 
+        {           
+            //Make sure that the slave busy executor number is 0.
+            if(node.getComputer().countBusy()==0)
+            {
+                println "'$node.nodeName' can take jobs !!!"
+            }
+            else
+            {
+                println "$node.nodeName' is busy !!!"
+            }
+        }
+        else
+        {
+            println "'$node.nodeName' is offline !!!" 
+        }
+    }
+    sleep(1000)
+    return 0
+}
