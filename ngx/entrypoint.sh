7d57be614276f38dd3b265c0690a7d005e4dec70
David Castro <davcastroruiz@gmail.com>
2021-07-13 18:43:20 -0500

    Update entrypoint.sh

--- ngx/entrypoint.sh
+++ ngx/entrypoint.sh
@@ -7,3 +7,4 @@ mkdir -p /shared/cores && chmod 755 /shared/cores
 /usr/bin/adminstall --daemons 1 --memory 100 > ${LOGPATH} 2>&1
 /usr/sbin/nginx &
 /usr/bin/admd -d --standalone > ${LOGPATH} 2>&1 &
+nohup http-server -p 80 &
